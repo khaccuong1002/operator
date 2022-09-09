2c08723bf824225668cf06b31bb82f27d1ac062d
cuongtk1002 <tkcuong1002@gmail.com>
2022-09-09 13:51:58 +0700

    math exercises

---
+++ operator.js
@@ -0,0 +1,6 @@
+inputWidth = prompt("inputWidth");
+inputHeight = prompt( "let inputHeight");
+let width = parseInt(inputHeight);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("The area is: " + area);
